5e0fdd8d85b1cdb7200a7bb06b1604bffe201de3
=Salman nazeer <salmanselu2002@gmail.com>
2022-01-30 18:49:23 +0530

    new dashoard

--- index.js
+++ index.js
@@ -23,11 +23,55 @@ app.get('/', (req, res) => {
     return client
       .query('select * from members order by points desc;')
       .then(resp=>{
-        let text = ""
+        let text = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>CUBE</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+        
+        
+          <!--Header Starts-->
+        
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <div>
+                  <img src="logo.png" class="logo"> <div class="text">PROTOCUBE</div>
+                </div> 
+                
+                <!-- Search Bar -->
+        
+                <!-- <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-success" type="submit"> <i class="bi bi-search"></i> </button>
+                </form> -->
+        
+            </div>
+          </nav>
+        `
         for(var i = 0; i <resp.rowCount; i++)
         {
-          text += `<img src="${resp.rows[i].avatarurl}"> ${resp.rows[i].points} ${resp.rows[i].tag}<br>`;
+          text += ` <div class="card mb-3 container-fluid" style="max-width: 540px;">
+                      <div class="row g-0">
+                        <div class="col-3">
+                          <img src="${resp.rows[i].avatarurl}" class="img-fluid rounded-start">
+                        </div>
+                        <div class="col-8">
+                          <div class="card-body">
+                            <h5 class="card-title">${resp.rows[i].tag}</h5>
+                            <p class="card-text">POINTS: ${resp.rows[i].points}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>`
         }
+        text += `</body></html>`;
         client.release();
         res.set("Access-Control-Allow-Origin", "*");
         res.send(text);
